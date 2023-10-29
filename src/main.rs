85f4dc5b0114733adbe6bfda57320b584eb22b10
merlin <kvasjir@proton.me>
2023-10-29 17:25:19 +0100

    chapter 14

--- src/main.rs
+++ src/main.rs
@@ -13,60 +13,78 @@ use camera::Camera;
 use color::Color;
 use hittable::{hittable_list::HittableList, sphere::Sphere};
 use material::*;
+use rand::Rng;
 use vec3::*;
 
 type Num = f64;
 
 fn main() {
+    let mut rand = rand::thread_rng();
     // World
     let mut world = HittableList::new();
-    let material_ground = Rc::new(materials::Lambertian::new(&Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(materials::Lambertian::new(&Color::new(0.1, 0.2, 0.5)));
-    let material_glass = Rc::new(materials::Dielectric::new(&1.5));
-    let material_right = Rc::new(materials::Metal::new(&Color::new(0.8, 0.6, 0.2), 0.0));
-
+    let groud_material = Rc::new(materials::Lambertian::new(&Color::new(0.5, 0.5, 0.5)));
     world.add(Box::new(Sphere::new(
-        Point3::new(0., -100.5, -1.),
-        100.,
-        material_ground.clone(),
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(0., 0., -1.),
-        0.5,
-        material_center.clone(),
+        Point3::new(0., -1000., 0.),
+        1000.,
+        groud_material.clone(),
     )));
 
-    // Hollow glass sphere
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand.gen::<Num>();
+            let center = Point3::new(
+                a as Num + 0.9 * rand.gen::<Num>(),
+                0.2,
+                b as Num + 0.9 * rand.gen::<Num>(),
+            );
+
+            if (center - Point3::new(4., 0.2, 0.)).lenght() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random() * &Color::random();
+                    let sphere_material = Rc::new(materials::Lambertian::new(&albedo));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.);
+                    let fuzz = rand.gen::<Num>();
+                    let sphere_material = Rc::new(materials::Metal::new(&albedo, fuzz));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    // glass
+                    let sphere_material = Rc::new(materials::Dielectric::new(&1.5));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(materials::Dielectric::new(&1.5));
+    world.add(Box::new(Sphere::new(Vec3::new(0., 1., 0.), 1.0, material1)));
+
+    let material2 = Rc::new(materials::Lambertian::new(&Color::new(0.4, 0.2, 0.1)));
     world.add(Box::new(Sphere::new(
-        Point3::new(-1., 0., -1.),
-        0.5,
-        material_glass.clone(),
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-1., 0., -1.),
-        -0.4,
-        material_glass.clone(),
+        Vec3::new(-4., 1., 0.),
+        1.0,
+        material2,
     )));
 
-    world.add(Box::new(Sphere::new(
-        Point3::new(1., 0., -1.),
-        0.5,
-        material_right.clone(),
-    )));
+    let material3 = Rc::new(materials::Metal::new(&Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(Vec3::new(4., 1., 0.), 1.0, material3)));
 
     let mut cam = Camera::default();
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = std::f64::consts::PI / 9.0;
-    cam.lookfrom = Point3::new(-2., 2., 1.);
-    cam.lookat = Point3::new(0., 0., -1.);
+    cam.lookfrom = Point3::new(13., 2., 3.);
+    cam.lookat = Point3::new(0., 0., 0.);
     cam.vup = Vec3::new(0., 1., 0.);
 
-    cam.defocus_angle = std::f64::consts::PI / 18.;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = std::f64::consts::PI / 300.;
+    cam.focus_dist = 10.0;
 
     let mut f = fs::File::create("./render.ppm").unwrap();
     cam.render(&mut f, &world);
